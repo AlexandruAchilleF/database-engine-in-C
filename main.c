f425cbd3637bfec32842e34c2dac18f6406b11a0
AlexandruAchilleF <achilealex@gmail.com>
2026-02-10 17:25:57 +0200

    Added prototypes for insert and select handling

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include<string.h>
 #include<stdbool.h>
 
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
 typedef struct{
     char *buffer;
     size_t buffer_length;
@@ -10,14 +12,36 @@ typedef struct{
 }InputBuffer;
 
 typedef struct {
-    char *username;
-    char *email;
+    int id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
 }User;
 
 typedef enum {
     META_COMMAND_SUCCESS,
-    META_COMMAND_UNRECOGNIZED_COMMAND
+    META_COMMAND_UNRECOGNIZED
 } MetaCommandResult;
+
+typedef enum {
+    PREPARE_SUCCESS,
+    PREPARE_SYNTAX_ERROR,
+    PREPARE_UNRECOGNIZED_STATEMENT
+}PrepareResult;
+
+typedef enum {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+}StatementType;
+
+typedef enum {
+    EXECUTE_SUCCESS,
+    EXECUTE_ERROR
+}ExecuteResult;
+
+typedef struct {
+    StatementType statement_type;
+    User row_to_insert;//For insert statements only
+}Statement;
 
 InputBuffer *new_input_buffer() {
     InputBuffer *input_buffer = malloc(sizeof(InputBuffer));
@@ -57,12 +81,50 @@ MetaCommandResult do_meta_command(InputBuffer *input_buffer) {
     if (strcmp(input_buffer->buffer, ".help") == 0) {
         printf("\n------- Help Menu -------\n");
         printf(".exit   : Exits the database\n");
-        printf(".help   : Print the help menu\n");
+        printf(".help   : Prints the help menu\n");
+        printf("insert \"username\" \"email\" : Inserts a new user \n");
+        printf("select  : Prints database content\n");
         printf("----------------------------\n\n");
         return META_COMMAND_SUCCESS;
     }
+    return META_COMMAND_UNRECOGNIZED;
+}
 
-    return META_COMMAND_UNRECOGNIZED_COMMAND;
+PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement) {
+    if (strncmp(input_buffer->buffer,"insert", 6)==0) {
+        statement->statement_type = STATEMENT_INSERT;
+        int argc = sscanf(input_buffer->buffer, "insert %s %s", statement->row_to_insert.username, statement->row_to_insert.email);
+        if (argc<2) {
+            return PREPARE_SYNTAX_ERROR;
+        }
+        return PREPARE_SUCCESS;
+    }
+    if (strncmp(input_buffer->buffer,"select", 6)==0) {
+        statement->statement_type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+ExecuteResult execute_insert(Statement *statement){
+    printf("Insert executed\n");
+    return EXECUTE_SUCCESS;
+}
+
+ExecuteResult execute_select(Statement *statement){
+    printf("Select executed\n");
+    return EXECUTE_SUCCESS;
+}
+
+ExecuteResult execute_statement(Statement *statement) {
+    switch (statement->statement_type) {
+        case(STATEMENT_INSERT) : {
+            return execute_insert(statement);
+        }
+        case(STATEMENT_SELECT) : {
+            return execute_select(statement);
+        }
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -75,12 +137,27 @@ int main(int argc, char* argv[]) {
             switch (do_meta_command(input_buffer)) {
                 case (META_COMMAND_SUCCESS):
                     continue;
-                case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                case (META_COMMAND_UNRECOGNIZED): {
                     printf("Unrecognized command '%s'.\n", input_buffer->buffer);
                     continue;
+                }
             }
         }
-        printf("Unrecognized command '%s'\n", input_buffer->buffer);
+        else {
+            Statement input_statement;
+            switch (prepare_statement(input_buffer, &input_statement)) {
+                case (PREPARE_SUCCESS) : break;
+                case (PREPARE_SYNTAX_ERROR) : {
+                    printf("Syntax error: '%s'.\n", input_buffer->buffer);
+                    continue;
+                }
+                    case (PREPARE_UNRECOGNIZED_STATEMENT) : {
+                    printf("Unrecognized command: '%s'.\n", input_buffer->buffer);
+                    continue;
+                }
+            }
+            execute_statement(&input_statement);
+        }
     }
     return 0;
 }
